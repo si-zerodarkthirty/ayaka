d9ccd11145781134f0e3eb8181df8ed82ae63bf4
si-zerodarkthirty <kyotan.equilibria@gmail.com>
2020-04-29 13:17:02 +0900

    init

--- components/Header.vue
+++ components/Header.vue
@@ -1,11 +1,12 @@
 <template>
   <header class="fixed p-5 w-full text-center">
     <h1 class="text-xl cursor-pointer" @click="$router.push('/')">codes&nodes.</h1>
-    <h2 class="text-xs">Shusuke IOKU; Waseda; Interstate Bargaining, Military Signals; <a href="https://twitter.com/o_dark_3o" target="_blank">@o_dark_3o</a>.</h2>
+    <h2 class="text-xs">Shusuke IOKU; Waseda; <a href="https://twitter.com/o_dark_3o" target="_blank">@o_dark_3o</a>.</h2>
     <input 
       class="appearance-none block w-full bg-transparent border-b border-black py-3 px-4 mx-auto leading-tight focus:outline-none text-center" 
       type="text"
       v-model="search"
+      placeholder="Interstate Bargaining, Military Signals"
       @keypress.enter="$router.push('/search/' + search)"
     >
   </header>
